95c8f76a60b0362c2092082560d697892aded7c6
Dan Moore <9156191+drmrd@users.noreply.github.com>
2026-06-10 19:39:57 -0400

    feat: pick a finish and dispatch the surface assignment

---
+++ editor/paint/finish-picker.tsx
@@ -0,0 +1,28 @@
+import { assignSurfacePaint, builtinFinishes, type Color, type SurfaceRef } from '../../core'
+
+export interface FinishPickerProps {
+  surface: SurfaceRef
+  color: Color
+  finishId: string
+  dispatch: (command: ReturnType<typeof assignSurfacePaint>) => void
+}
+
+export function FinishPicker({ surface, color, finishId, dispatch }: FinishPickerProps) {
+  return (
+    <fieldset>
+      <legend>Finish</legend>
+      {Object.keys(builtinFinishes.entries).map((id) => (
+        <label key={id}>
+          <input
+            type="radio"
+            name="finish"
+            value={id}
+            checked={id === finishId}
+            onChange={() => dispatch(assignSurfacePaint(surface, color, id))}
+          />
+          {id}
+        </label>
+      ))}
+    </fieldset>
+  )
+}
